24d4f2d93ec20a7ba1bde1e98d22938724d77b73
Yasmina <kezimanareineyasmina@yahoo.fr>
2018-02-01 12:35:02 +0400

    commit initial après la création du projet.
    aucune modification n'a encore été faite sur le projet.

---
+++ src/main/java/com/udm/udmapp/UdmappBackendApplication.java
@@ -0,0 +1,12 @@
+package com.udm.udmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdmappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdmappBackendApplication.class, args);
+	}
+}
